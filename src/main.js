01281cebf97de383fe6a8efdd052ff81a432aa5c
BigJman123 <BigJman1234567@yahoo.com>
2017-05-08 10:13:34 -0500

    finished 'awesome object enhancements'

--- src/main.js
+++ src/main.js
@@ -1,25 +1,98 @@
-let name = 'Foo';
+// object shorthand
 
-let template = `
-	<div class ="Alert">,
+// function getPerson() {
+// 	let name = 'John';
 
-		<p>${name}</p>,
+// 	let age = 25;
 
-	</div>
-`;
+// 	return {
+// 		name,
 
-console.log(template);
+// 		age,
+
+// 		greet() { // ES6
+// 			return `Hello, ${this.name}`;
+
+// 			// return 'Hello, ' + this.name;
+// 		}
+
+// 	};
+// }
+
+// alert(getPerson().greet());
+
+// let { name, age } = person;
+
+// alert(name);
 
 
-// not sure what Vue js is but i copied this anyway
-// new Vue({
-// 	template: `
 
-// 		<div class="Alert">
-			
-// 			<p>Foobar</p>
 
-// 		</div>
 
-// `;
+// let data = {
+// 	name: 'Karen',
+// 	age: 32,
+// 	results: ['foo', 'bar'],
+// 	count: 30
+// };
+
+// let {results, count } = data;
+
+// console.log(results, count);
+
+
+
+
+
+// function getData({results, count}) {
+// 	// var results = data.results; // ES5
+// 	// var count = data.count;
+
+// 	console.log(results, count);
+// }
+
+// getData({
+// 	name: 'Karen', 
+// 	age: 32,
+// 	results: ['foo', 'bar'],
+// 	count: 30
+// });
+
+
+
+// This is the ES5 way
+// function greet(person) {
+// 	let name = person.name;
+// 	let age = person.age;
+
+// 	console.log('Hello, ' + name + '. You are ' + age);
+// }
+
+// greet({
+// 	name: 'Luke',
+// 	age: 24
 // })
+
+
+// This is the ES6 way
+function greet({name, age}) {
+	console.log(`Hello, ${name}. You are ${age}.`);
+}
+
+greet({
+	name: 'Luke',
+	age: 24
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
